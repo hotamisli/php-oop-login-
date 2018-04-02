27a384cab81d497ec6e65d8d8bed8bd5135a0ff9
hotamisli <salihahotamisli@gmail.com>
2018-04-02 15:08:38 +0000

    login system

---
+++ core/init.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+
+$GLOBALS['config']=array(
+    'mysql'=>array(
+      'host'=>'127.0.0.1' ,
+      'username'=>'root',
+      'password'=>'',
+      'db'=>'login'
+    ),
+
+    'remember'=>array(
+        'cookie_name'=>'hash',
+        'cookie_expiry'=>644800
+    ),
+
+    'session'=>array(
+        'session_name'=>'user'
+    )
+
+);
+
+spl_autoload_register(function($class){
+    require_once 'classes/'.$class. '.php';
+});
+
+require_once 'functions/sanitize.php';
